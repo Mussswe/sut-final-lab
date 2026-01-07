37b9d84eb8868189a8080efd434f8e6665b7cc19
Mussswe <lylot0t66@gmail.com>
2026-01-07 12:15:07 +0700

    close #2

---
+++ backend/entity/c6600013_nagative_price_test.go
@@ -0,0 +1,21 @@
+package entity
+
+import (
+	"testing"
+	"github.com/onsi/gomega"
+)
+
+func TestPostive(t *testing.T) {
+	g := NewWithT(t)
+    
+	t.Run("positevetest") {(
+	b = Books{
+		Title: "ASDdssdf",
+		Price: 0.00,
+		Code:  "BK123456",
+	}
+	var err,valid = sadhgj
+
+	g.Expect(b.err()).NotToBe(nil)
+    )}
+}
